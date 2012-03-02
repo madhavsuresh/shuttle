ccf1dc3df28ce55cc3b94b63e4e21211be35580f
Madhav <madman92@gmail.com>
2012-03-01 23:20:51 -0600

    initial commit

---
+++ src/shuttle/hci/com/ShuttleActivity.java
@@ -0,0 +1,13 @@
+package shuttle.hci.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShuttleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
