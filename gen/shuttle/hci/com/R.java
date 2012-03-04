f14cabf1e741b101b0946f64f26c8b0b33b7ccec
dennis <dennis@Notebook-X220.(none)>
2012-03-04 14:49:47 -0600

    Created basic layout for home page

--- gen/shuttle/hci/com/R.java
+++ gen/shuttle/hci/com/R.java
@@ -13,8 +13,17 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int departureTime=0x7f050004;
+        public static final int destination=0x7f050002;
+        public static final int destinationLabel=0x7f050000;
+        public static final int destinationSublabel=0x7f050001;
+        public static final int go=0x7f050005;
+        public static final int timeLabel=0x7f050003;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int list_item=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
